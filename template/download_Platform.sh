7c8814df7008c55b7193ef1464b774a9ae790d43
Li Yang <1175529741@qq.com>
2022-01-31 17:14:47 +0800

    template

---
+++ template/download_Platform.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -d Platform ]; then
+    rm -rf Platform
+fi
+
+git clone git@github.com:LeeYoungCN/Platform.git
+
+root=$(pwd)
+platform_path=$(cd Platform;pwd)
+cd ${platform_path}/build
+./init_env.sh
+cd ${root}
+if [ -d ${root}/.vscode ]; then
+    mkdir -p ${root}/.vscode
+fi
+
+if [ -e ${root}/.vscode/task.json ];then
+    cp -rf ${platform_path}/template/task.json ${root}/.vscode/
+fi
