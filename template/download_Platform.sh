92cdb6bdb16fb4f4c6cf6471cea4ce115b1a15c8
Li Yang <1175529741@qq.com>
2022-01-31 18:13:42 +0800

    download_shell

--- template/download_Platform.sh
+++ template/download_Platform.sh
@@ -10,14 +10,19 @@ platform_path=$(cd Platform;pwd)
 cd ${platform_path}/build
 ./init_env.sh
 cd ${root}
+
+function copy_tmplate_file()
+{
+    if [ ! -e ${root}/${1} ];then
+        cp -rf ${platform_path}/template/${1} ${root}/${1}
+    fi
+}
+
 if [ ! -d ${root}/.vscode ]; then
     mkdir -p ${root}/.vscode
 fi
 
-if [ ! -e ${root}/.vscode/tasks.json ];then
-    cp -rf ${platform_path}/template/tasks.json ${root}/.vscode/
-fi
-
-if [ ! -e ${root}/CMakeLists.txt ];then
-    cp -rf ${platform_path}/template/CMakeLists.txt ${root}/CMakeLists.txt
-fi
+copy_tmplate_file ".vscode/tasks.json"
+copy_tmplate_file "CMakeLists.txt"
+copy_tmplate_file "download_Platform.sh"
+copy_tmplate_file ".gitignore"
