c9a09daefd256ea875df384d13b9e64541e271fc
Li Yang <1175529741@qq.com>
2022-01-31 17:18:20 +0800

    template

--- template/download_Platform.sh
+++ template/download_Platform.sh
@@ -10,10 +10,14 @@ platform_path=$(cd Platform;pwd)
 cd ${platform_path}/build
 ./init_env.sh
 cd ${root}
-if [ -d ${root}/.vscode ]; then
+if [ ! -d ${root}/.vscode ]; then
     mkdir -p ${root}/.vscode
 fi
 
-if [ -e ${root}/.vscode/task.json ];then
+if [ ! -e ${root}/.vscode/task.json ];then
     cp -rf ${platform_path}/template/task.json ${root}/.vscode/
+fi
+
+if [ ! -e ${root}/CMakeLists.txt ];then
+    cp -rf ${platform_path}/template/CMakeLists.txt ${root}/CMakeLists.txt
 fi
