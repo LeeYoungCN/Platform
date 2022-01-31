63f8e88925a524b49506358ba8a03a79ad6e11b9
Li Yang <1175529741@qq.com>
2022-01-31 23:06:30 +0800

    download

--- template/download_Platform.sh
+++ template/download_Platform.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+function copy_tmplate_file()
+{
+    file_name="${1}"
+    file_name=${file_name#${template_path}\/}
+    if [ ! -e ${root}/${file_name} ];then
+        cp -rf ${template_path}/${file_name} ${root}/${file_name}
+    fi
+}
+
+function copy_all_file()
+{
+    for f in $(ls -a $1); do
+        if [ ${f} == "." -o ${f} == ".." ]; then
+            continue
+        fi
+        if [ -d ${f} ]; then
+            copy_all_file "${1}/${f}"
+        else
+            copy_tmplate_file "${1}/${f}"
+        fi
+    done
+}
+
 if [ -d Platform ]; then
     rm -rf Platform
 fi
@@ -7,22 +30,13 @@ git clone git@github.com:LeeYoungCN/Platform.git
 
 root=$(pwd)
 platform_path=$(cd Platform;pwd)
+template_path="${platform_path}/template"
 cd ${platform_path}/build
 ./init_env.sh
 cd ${root}
-
-function copy_tmplate_file()
-{
-    if [ ! -e ${root}/${1} ];then
-        cp -rf ${platform_path}/template/${1} ${root}/${1}
-    fi
-}
 
 if [ ! -d ${root}/.vscode ]; then
     mkdir -p ${root}/.vscode
 fi
 
-copy_tmplate_file ".vscode/tasks.json"
-copy_tmplate_file "CMakeLists.txt"
-copy_tmplate_file "download_Platform.sh"
-copy_tmplate_file ".gitignore"
+copy_all_file ${template_path}
