8cace340c2f30158aec24bbdb7c039a2452f2c8b
Li Yang <1175529741@qq.com>
2022-02-01 23:35:17 +0800

    set launch.json

--- build/move_sys_cfg_file.sh
+++ build/move_sys_cfg_file.sh
@@ -5,6 +5,6 @@ source ./public_shell_func.sh
 cp -rf ./../os_cfg/${os}/* ${cmake_source_dir}/
 cp -rf ./../os_cfg/${os}/.vscode/* ${cmake_source_dir}/.vscode/
 
-if [ ${#taeget_name} -gt 0 ]; then
+if [ ${#target_name} -gt 0 ]; then
     set_exe_path_in_launch
 fi
