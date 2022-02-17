7d0ab8cabfbfb905276fcc9b4f02025274522b21
Li Yang <1175529741@qq.com>
2022-02-17 23:36:30 +0800

    git pull

--- template/download_Platform.sh
+++ template/download_Platform.sh
@@ -7,18 +7,7 @@ git_branch=""
 git_zip=""
 git_zip_link=""
 
-function get_git_info()
-{
-    git_ssh="${1}"
-    git_branch="${2}"
-    git_user=${git_ssh#git@github.com:}
-    git_user=${git_user%/*}
-    git_repository=${git_ssh#*/}
-    git_repository=${git_repository%*.git}
-    git_zip="${git_repository}-${git_branch}.zip"
-    git_zip_link="https://github.com/${git_user}/${git_repository}/archive/refs/heads/${git_branch}.zip"
-}
-
+##### log 打印 #####
 function print_log()
 {
     d=$(date "+%Y-%m-%d %H:%M:%S")
@@ -31,6 +20,45 @@ function result_log()
         print_log "$2 success!"
     else
         print_log "$2 fail!"
+    fi
+}
+
+##### 备份文件处理 #####
+function backup_file()
+{
+    file="${1}"
+    if [ -e ${file} ]; then
+        mv -f "${file}" "${file}_backup"
+        result_log $? "back up ${file}"
+    fi
+}
+
+function reset_from_backup()
+{
+    file="${1}"
+    if [ -e "${file}_backup" ]; then
+        mv -f "${file}_backup" "${file}"
+        result_log $? "reset  ${file}_backup -> ${file}"
+    fi
+}
+
+function delete_backup()
+{
+    file="${1}"
+    if [ -e "${file}_backup" ]; then
+        rm -rf "${file}_backup"
+        result_log $? "delete  ${file}_backup"
+    fi
+}
+
+function process_backup()
+{
+    result=${1}
+    folder=${2}
+    if [ ${result} -eq 0 ]; then
+        delete_backup "${folder}"
+    else
+        reset_from_backup "${folder}"
     fi
 }
 
@@ -51,50 +79,52 @@ function copy_all_file()
     done
 }
 
-function backup_file()
+##### git 仓处理 #####
+function get_git_info()
 {
-    file="${1}"
-    if [ -e ${file} ]; then
-        mv -f "${file}" "${file}_backup"
-    fi
-}
-
-function reset_from_backup()
-{
-    file="${1}"
-    if [ -e "${file}_backup" ]; then
-        mv -f "${file}_backup" "${file}"
-    fi
-}
-
-function delete_backup()
-{
-    file="${1}"
-    if [ -e "${file}_backup" ]; then
-        rm -rf "${file}_backup"
-    fi
+    git_ssh="${1}"
+    git_branch="${2}"
+    git_user=${git_ssh#git@github.com:}
+    git_user=${git_user%/*}
+    git_repository=${git_ssh#*/}
+    git_repository=${git_repository%*.git}
+    git_zip="${git_repository}-${git_branch}.zip"
+    git_zip_link="https://github.com/${git_user}/${git_repository}/archive/refs/heads/${git_branch}.zip"
 }
 
 function git_clone_repository()
 {
     ssh_path="${1}"
-    branch=${2}
+    branch="${2}"
     repository=${ssh_path#*/}
     repository=${repository%*.git}
-    backup_file "${repository}"
     log_str="git clone ${ssh_path}"
+    # 优先git pull
+    if [ -e "${repository}" ]; then
+        pushd ./${repository} >> /dev/null
+        git checkout ${branch}
+        git pull
+        result=$?
+        result_log "${result}" "git pull ${repository}"
+        popd >> /dev/null
+        if [ ${result} -eq 0 ]; then
+            return 0
+        fi
+    fi
+    # git pull 失败则进行git clone
+    backup_file "${repository}"
     git clone "${ssh_path}"
-    if [ $? -ne 0 ]; then
-        result_log 1 "${log_str}"
-        reset_from_backup "${repository}"
+    result=$?
+    result_log ${result} "${log_str}"
+    process_backup ${result} "${repository}"
+    if [ ${result} -ne 0 ]; then
         return 1
     fi
+
     chmod -R 700 ${repository}
     pushd ./${repository}
     git checkout ${branch}
     popd
-    result_log 0 "${log_str}"
-    delete_backup "${repository}"
     return 0
 }
 
@@ -103,25 +133,23 @@ function unzip_file()
     zip_file=${1}
     unzip_file=${2}
     log_str="unzip ${zip_file}"
-    if [ ! -e ${zip_file} ]; then
+    if [ ! -e "${zip_file}" ]; then
         print_log "${zip_file} not exist!"
         return 1
     fi
-    if [ -e ${unzip_file} ]; then
+    if [ -e "${unzip_file}" ]; then
         backup_file "${unzip_file}"
     fi
 
-    unzip -o ${zip_file} >> /dev/null
-    if [ $? -eq 0 ]; then
-        result_log 0 "${log_str}"
+    unzip -o ${zip_file}
+    result=$?
+    if [ ${result} -eq 0 ]; then
         mv -f "${zip_file%%.zip}" "${unzip_file}"
         chmod -R 700 "${unzip_file}"
-        delete_backup "${unzip_file}"
-        return 0
     fi
-    result_log 1 "${log_str}"
-    reset_from_backup ${unzip_file}
-    return 1
+    result_log      "${result}" "${log_str}"
+    process_backup  "${result}" "${unzip_file}"
+    return ${result}
 }
 
 function download_file()
@@ -133,14 +161,13 @@ function download_file()
         backup_file ${target}
     fi
     wget -O ${target} ${link}
-    if [ $? -ne 0 ]; then
-        result_log 1 "${log_str}"
-        rm -rm ${target}
-        reset_from_backup ${target}
+    result=$?
+    result_log "${result}" "${log_str}"
+    if [ ${result} -ne 0 ]; then
+        rm -rf ${target}
         return 1
     fi
-    result_log 0 "${log_str}"
-    delete_backup ${target}
+    process_backup  "${result}" "${target}"
     return 0
 }
 
